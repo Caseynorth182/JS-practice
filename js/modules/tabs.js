51bf8ae6729a5cfaf8d88f83141de8f22f3bad4b
CaseyNorthe <caseynorth182@gmail.comm>
2022-03-23 09:20:22 -0700

    first commit

---
+++ js/modules/tabs.js
@@ -0,0 +1,43 @@
+function tabs(tabsSelector, tabsContents, tabsParents, activeClass) {
+    // Tabs
+
+    let tabs = document.querySelectorAll(tabsSelector),
+        tabsContent = document.querySelectorAll(tabsContents),
+        tabsParent = document.querySelector(tabsParents);
+
+    function hideTabContent() {
+
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove(activeClass);
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add(activeClass);
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', function(event) {
+        const target = event.target;
+        if (target && target.classList.contains(tabsSelector.slice(1))) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+}
+
+
+export default tabs
